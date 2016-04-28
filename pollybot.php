c4a3f3ac36e7cd587edade198bac3d108ce06b3b
zedkay22 <zkarimibots@gmail.com>
2016-04-27 21:03:03 -0400

    addingthis

---
+++ pollybot.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+$botToken = "165180744:AAHG_JvmlUf0icqbKgtmfRa8j5jVyDY2yUc"; 
+$website = "https://api.telegram.org/bot".$botToken; 
+
+$update = file_get_contents($website."/getupdates");
+
+print_r($update);
+
+
+?> 
